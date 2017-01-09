9c08358c18bcb21cca256cca30b4e26b46d4f80b
Mateusz Nowakowski <mateusz.nowakowski@nokia.com>
2017-01-09 12:21:29 +0100

    netctl configs

--- i3/3_screens.sh
+++ i3/3_screens.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output VIRTUAL1 --off --output DP3 --off --output eDP1 --mode 1600x900 --pos 3840x0 --rotate normal --output DP1 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI3 --off --output HDMI2 --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI1 --off --output VGA1 --off --output DP2 --off
+xrandr --output VIRTUAL1 --off --output DP3 --off --output eDP1 --mode 1600x900 --pos 3840x0 --rotate normal --output DP1 --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI3 --off --output HDMI2 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI1 --off --output VGA1 --off --output DP2 --off
--- zshrc
+++ zshrc
@@ -3,6 +3,6 @@ source $HOME/config/profile
 source $HOME/config/bookmarks
 source $HOME/config/functions
 source $HOME/config/zsh-config
-if [ -e $HOME/.zsh-config ]; then
-    source $HOME/.zsh-config
+if [ -e $HOME/zsh-config ]; then
+    source $HOME/zsh-config
 fi
