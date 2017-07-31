9709c3a3779372fb9f161a14fcdf32e689496b37
Mateusz Nowakowski <mateusz.nowakowski@nokia.com>
2017-07-31 11:02:46 +0200

    fix

--- i3/3_screens.sh
+++ i3/3_screens.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output VIRTUAL1 --off --output DP3 --off --output eDP1 --primary --mode 1600x900 --pos 1120x1200 --rotate normal --output DP1 --mode 1920x1200 --pos 0x0 --rotate normal --output HDMI3 --off --output HDMI2 --mode 1920x1200 --pos 1920x0 --rotate normal --output HDMI1 --off --output VGA1 --off --output DP2 --off
+xrandr --output VIRTUAL1 --off --output DP3 --off --output eDP1 --primary --mode 1600x900 --pos 1120x1200 --rotate normal --output DP1 --mode 1920x1080 --pos 0x120 --rotate normal --output HDMI3 --off --output HDMI2 --mode 1920x1200 --pos 1920x0 --rotate normal --output HDMI1 --off --output VGA1 --off --output DP2 --off
--- zshrc
+++ zshrc
@@ -6,3 +6,6 @@ source $HOME/config/zsh-config
 if [ -e $HOME/zsh-config ]; then
     source $HOME/zsh-config
 fi
+#autoload edit-command-line
+#zle -N edit-command-line
+#bindkey '^Xe' edit-command-line
