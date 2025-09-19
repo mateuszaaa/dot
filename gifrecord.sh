76fba5062ef9d6cb1fc84110f832d2518f347ac1
Mateusz Nowakowski <mat13mn@gmail.com>
2025-09-19 13:07:10 +0200

    macbook switch

--- gifrecord.sh
+++ gifrecord.sh
@@ -17,16 +17,18 @@ main() {
 
   while [ "$STOP_LOOP" = false ]; do
     grim -g "$GEOMETRY" $DIR/$(printf "%04d" $i).png
+    convert $DIR/$(printf "%04d" $i).png -colors 128 $DIR/$(printf "%04d" $i).png
     ((i = i + 1)) # Increment i
     sleep 0.2
   done
 
   rm -rf /tmp/ss.gif || true
   ffmpeg -framerate "5" -i "$DIR"/%04d.png -vf "palettegen" $DIR/palette.png
-  ffmpeg -framerate "5" -i "$DIR"/%04d.png -i $DIR/palette.png -lavfi "paletteuse=dither=bayer" $DIR/ss.gif
+  ffmpeg -framerate "5" -i "$DIR"/%04d.png -i $DIR/palette.png -lavfi "paletteuse=dither=bayer" -r 10 $DIR/ss.gif
   cp $DIR/ss.gif /tmp/ss.gif
   wl-copy --type image/gif </tmp/ss.gif
-  notify-send '/tmp/ss.gif copied to clipboard',
+  notify-send "/tmp/ss.gif copied to clipboard"
+
 }
 
 # Execute main function
